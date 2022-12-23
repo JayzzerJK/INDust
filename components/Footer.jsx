bcfe1785a5d3e7cb66d4618745b11dcdc556f106
JayzzerJK <jayzzers_team@mail.ru>
2022-12-24 01:27:36 +0900

    Initial commit

---
+++ components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai';
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p>2022 INDust. Все права защищены.</p>
+      
+      <p className="icons">
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
+  )
+}
+
+export default Footer
