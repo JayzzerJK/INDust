261805bbd5d80c8a315faca96eb67f32594e092b
JayzzerJK <70437510+JayzzerJK@users.noreply.github.com>
2022-12-24 10:29:52 +0900

    Update Layout.jsx

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Head>
-        <title>JS Mastery Store</title>
+        <title>INDust</title>
       </Head>
       <header>
         <Navbar />
